6943ecb2380db160ee38d063ea26ac38f28955ec
Hugo Boichard <71222348+HugoBoichard@users.noreply.github.com>
2021-08-09 12:17:23 +0200

    rasberry support

--- src/distribution.ts
+++ src/distribution.ts
@@ -1,7 +1,40 @@
 import { ipcMain } from "electron";
+import { Gpio } from "onoff";
+import * as _doses from "./data/doses.json";
+import * as _drinks from "./data/drinks.json";
+
+interface Pins {
+    [index: number]: any
+}
+interface Doses {
+    [key: string]: number
+}
+interface Drink {
+    name: string,
+    logo: string,
+    color: string,
+    pin: number
+}
+
+const isWin = process.platform === "win32";
+const doses = _doses as Doses;
+const drinks = _drinks as Drink[];
+let pins: Pins = {};
+
+// Pins initialisation
+if (!isWin) {
+    drinks.forEach(drink => {
+        pins[drink.pin] = new Gpio(drink.pin, 'high');
+    });
+}
 
 ipcMain.on('distribution', (event, arg) => {
-    event.reply('distribution-finish', 'finish');
+    if (isWin) {
+        return event.reply('distribution-finish', 'finish');
+    }
+    pins[arg.drink.pin].writeSync(Gpio.LOW);
+    setTimeout(() => {
+        pins[arg.drink.pin].writeSync(Gpio.HIGH);
+        event.reply('distribution-finish', 'finish');
+    }, doses[arg.quantity] * 1000);
 });
-
-console.log("En attente de distribution ...");
